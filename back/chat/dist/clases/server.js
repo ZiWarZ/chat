09c4d315f61518e491cfbc0999497c74bee430e5
ZiWarZ <46983349+ZiWarZ@users.noreply.github.com>
2019-03-26 19:28:13 -0500

    Proyecto inicial con configuracion de socket en front y back

---
+++ back/chat/dist/clases/server.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = __importDefault(require("socket.io"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+        this.httpServer = new http_1.default.Server(this.app);
+        this.io = socket_io_1.default(this.httpServer);
+        this.puerto = process.env.PORT || 3700;
+    }
+    start() {
+        this.httpServer.listen(this.puerto, () => {
+            console.log("Servidor iniciado correctamente... puerto=>" + this.puerto);
+        });
+    }
+}
+exports.default = Server;
---
+++ back/chat/index.ts
@@ -0,0 +1,5 @@
+import Server from './clases/server';
+import {router} from './routes/router';
+const server=new Server();
+server.app.use('/',router);
+server.start();
